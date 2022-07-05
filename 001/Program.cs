e57a6e30ff3e999f091ef20463ba0a8a6b082037
katya <hellifire@mail.ru>
2022-07-05 12:49:51 +0300

    success

--- 001/Program.cs
+++ 001/Program.cs
@@ -5,3 +5,7 @@
 string[] str = {"hello", "2", "world", ":-)"};
 for (int i = 0; i < str.Length; i++)
      Console.WriteLine("AS[{0}] = {1}", i, str[i]);
+     for (int i = 0; i < str.Length; i++)
+    {if(str[i].Length <= 3)
+      Console.WriteLine (str[i]);
+    }
